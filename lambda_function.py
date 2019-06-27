07da8c459c1c43c7e087012f8e9446c0352eb699
Vasilis Ventirozos <vasilis.ventirozos@credativ.us>
2019-06-27 16:11:56 +0300

    cleaning up

--- lambda_function.py
+++ lambda_function.py
@@ -12,11 +12,9 @@ def lambda_handler(event, context):
     slack = Slacker(slackapi)
     query_cmd = "select now()::timestamp(0),count(*) from pg_stat_activity;"
     print (query_cmd)
-    # get a connection, if a connect cannot be made an exception will be raised here
     conn = make_conn()
     result = fetch_data(conn, query_cmd)
     conn.close()
-    #slack.chat.post_message('@vasilis','activity count: ')
     message = json.dumps(result, indent=2, sort_keys=True, default=str)
     slack.chat.post_message('@vasilis', message)
     return result
